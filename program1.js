9f2c7e1e67dec8c9da6768f03e64f075ac33cfec
shivanshp2004 <shivanshpathak9988@gmail.com>
2024-10-07 15:12:27 +0530

    Shivansh Pathak

--- program1.js
+++ program1.js
@@ -3,9 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
